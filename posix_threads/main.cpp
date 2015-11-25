36a8cbed61e6743bd13a7bf42b79e5d9d65c3780
FranciscMoldovan <franciscmoldovan93@gmail.com>
2015-11-26 01:15:35 +0200

    doi

--- posix_threads/main.cpp
+++ posix_threads/main.cpp
@@ -1,35 +1,45 @@
-#include <iostream>
-
 extern "C"
 {
-#include <pthread.h>
-#include <unistd.h>
+#include<pthread.h>
+#include<unistd.h>
 }
-
+#include <iostream>
 using namespace std;
+const int NUMBER_OF_THREADS=5;
 
-void *function1(void *argument);
-void *function2(void *argument);
-
-int main(void)
+void *thread_talk(void *thread_number)
 {
-  pthread_t t1, t2; // declare two threads
-  // create threads
-  pthread_create(&t1, NULL, function1, NULL);
-  pthread_create(&t2, NULL, function2, NULL);
-
-  sleep(1);
-
-  return 0;
+    int a = *(static_cast<int*>(thread_number));
+    sleep(a+1);
+    cout << "Thread " << a << " has finished" << endl;
+    pthread_exit(NULL);
+return 0;
 }
-void *function1(void *argument)
+int main()
 {
-  cout << "hello ... " << endl ;
-  sleep(2); // fall asleep for two seconds
-  //pthread_exit(NULL);
+    pthread_t thread[NUMBER_OF_THREADS];
+    cout << "Starting all threads..." << endl;
+    int temp_arg[NUMBER_OF_THREADS];
+    for(int current_t=0; current_t<NUMBER_OF_THREADS; current_t++)
+    {
+        temp_arg[current_t]=current_t;
+        int result = pthread_create(&thread[current_t], NULL,thread_talk,static_cast<void*>(&temp_arg[current_t]));
+        if(result!=0)
+        {
+            cout << "Error creating thread " << current_t << ". Return code: " << result << endl;
+        }
+    }
+    for(int current_t=0; current_t<NUMBER_OF_THREADS; current_t++)
+    {
+        pthread_join(thread[current_t], NULL);
+    }
+    cout << "All threads completed.";
+return 0;
 }
-void *function2(void *argument)
-{
-  cout << " ... world" << endl;
-  pthread_exit(NULL);
-}
+
+
+
+
+
+
+
