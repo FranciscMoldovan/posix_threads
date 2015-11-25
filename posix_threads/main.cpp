309d371fb23a850ee5f2168365acafb953e2babf
FranciscMoldovan <franciscmoldovan93@gmail.com>
2015-11-26 01:50:51 +0200

    mutex & cond

--- posix_threads/main.cpp
+++ posix_threads/main.cpp
@@ -1,55 +1,98 @@
+#include <iostream>
 extern "C"
 {
-#include<pthread.h>
-#include<unistd.h>
+#include <pthread.h>
 }
-#include <iostream>
+
 using namespace std;
-const int NUMBER_OF_THREADS=5;
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t count_mutex     = PTHREAD_MUTEX_INITIALIZER;
 
-int val=10;
-void *thread_talk(void *thread_number)
+pthread_cond_t  condition_var   = PTHREAD_COND_INITIALIZER;
+
+void *function1(void *arg);
+void *function2(void *arg);
+
+int  count = 0;
+
+const int COUNT_DONE  = 10;
+
+main()
 {
-  for(int x = 1; x<500; x++)
+  pthread_t thread1, thread2;
+
+  pthread_create(&thread1, NULL, &function1, NULL);
+  pthread_create(&thread2, NULL, &function2, NULL);
+
+  pthread_join(thread1, NULL);
+  pthread_join(thread2, NULL);
+
+  cout << "Final count: " <<  count << endl;
+
+  return 0;
+}
+
+// Write numbers 1-3 and 8-10 as permitted by function2()
+
+void *function1(void *arg)
+{
+  // function1 (thread1) likes to add 1 to count regardless of it's current value,
+  // but will only do it if function2 is done working with count.
+
+  for(;;)
   {
-      // lock the shared value here
-      pthread_mutex_lock(&mutex);
-      val*=10;
-      val++;
-      val--;
-      val/=10;
-      // unlock it for the next thread:
-      pthread_mutex_unlock(&mutex);
+    // Lock mutex and then wait for signal to relase mutex
+    pthread_mutex_lock(&count_mutex);
+
+
+    // frunction1 gives control over count to function2 to see if function2 wants to do anything
+    // with count. It will unlock count until function2 signals it's done working with count.
+    pthread_cond_wait(&condition_var, &count_mutex);
+
+    // add this point function2 has given back control of count to funciton1 and
+    // has either added 1 to count or gave back control to function1 without changing count.
+    cout << "function1 looks at count (" << count << ") and just adds 1 to it." << endl;
+    count++;
+
+    pthread_mutex_unlock(&count_mutex);
+
+    if(count >= COUNT_DONE)
+    {
+      pthread_exit(NULL);
+    }
   }
-  pthread_exit(NULL);
 }
-int main()
+
+
+
+void *function2(void *arg)
 {
-    pthread_t thread[NUMBER_OF_THREADS];
-    cout << "Starting all threads..." << endl;
-    int temp_arg[NUMBER_OF_THREADS];
-    for(int current_t=0; current_t<NUMBER_OF_THREADS; current_t++)
+  // function 2 only likes to add 1 to odd numbers and will leave even numbers alone.
+
+  for(;;)
+  {
+    pthread_mutex_lock(&count_mutex);
+
+    if(count & 1)   // check if count is odd
     {
-        temp_arg[current_t]=current_t;
-        int result = pthread_create(&thread[current_t], NULL,thread_talk,static_cast<void*>(&temp_arg[current_t]));
-        if(result!=0)
-        {
-            cout << "Error creating thread " << current_t << ". Return code: " << result << endl;
-        }
+      cout << "function2 detected count (" << count << ") is odd, so it adds 1 to it." << endl;
+      count++;
     }
-    for(int current_t=0; current_t<NUMBER_OF_THREADS; current_t++)
+    else
     {
-        pthread_join(thread[current_t], NULL);
+
+      // function 2 detected the number is even, and will leave it alone and signal function1
+      // it has control back over count.
+      pthread_cond_signal(&condition_var);
+
     }
-    cout << "All threads completed.";
-return 0;
+
+    pthread_mutex_unlock(&count_mutex);
+
+    if(count >= COUNT_DONE)
+    {
+      pthread_exit(NULL);
+    }
+  }
+
 }
-
-
-
-
-
-
-
